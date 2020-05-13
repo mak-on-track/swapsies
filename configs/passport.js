d93534a6dba5a6740c548bf2a5af016accea2c3f
emelko <ml.kochsiek@mailbox.org>
2020-05-13 16:57:26 +0200

    Trial-set-up-backend-with-auth

---
+++ configs/passport.js
@@ -0,0 +1,43 @@
+console.log("BEEYYYYYYYYYYY........")
+
+const bcrypt         = require('bcryptjs');
+const passport       = require('passport');
+const LocalStrategy  = require('passport-local').Strategy;
+const User           = require('../models/User');
+
+passport.serializeUser((loggedInUser, cb) => {
+  cb(null, loggedInUser._id);
+});
+
+passport.deserializeUser((userIdFromSession, cb) => {
+  User.findById(userIdFromSession, (err, userDocument) => {
+    if (err) {
+      cb(err);
+      return;
+    }
+    cb(null, userDocument);
+  });
+});
+
+passport.use(
+  new LocalStrategy((username, password, next) => {
+    User.findOne({ username }, (err, foundUser) => {
+      if (err) {
+        next(err);
+        return;
+      }
+
+      if (!foundUser) {
+        next(null, false, { message: 'Incorrect username.' });
+        return;
+      }
+
+      if (!bcrypt.compareSync(password, foundUser.password)) {
+        next(null, false, { message: 'Incorrect password.' });
+        return;
+      }
+
+      next(null, foundUser);
+    });
+  })
+);
