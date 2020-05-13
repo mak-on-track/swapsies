7e21d72b9cf2c54276e757235d916b491a6333d6
Aleah Connley <aleahkc@gmail.com>
2020-05-13 21:39:07 +0200

    changes

--- models/Item.js
+++ models/Item.js
@@ -4,8 +4,8 @@ const { Schema, model } = mongoose;
 const ItemSchema = new Schema({
   name: String,
   img: String,
-  type: enum[("Service", "Item")],
-  category: enum[("Furniture", "Plants", "Food")],
+  type: enum [("Service", "Item")],
+  category: enum [("Furniture", "Plants", "Food", )],
   description: String,
   status: enum[("Available", "Pending", "Sold")],
   favourites: Number,
