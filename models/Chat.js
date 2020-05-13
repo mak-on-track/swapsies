acddd60ab4beec0aa9fced3ab6acd562f058023c
Aleah Connley <aleahkc@gmail.com>
2020-05-13 18:05:08 +0200

    models done

---
+++ models/Chat.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const ChatSchema = new Schema(
+  {
+    subject: String,
+    participant1: { type: Schema.Types.ObjectId, ref: "User" },
+    participant2: { type: Schema.Types.ObjectId, ref: "User" },
+    type: enum[("Service", "Item")],
+    messages: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        msg: String,
+        time: { type: Date, default: Date.now },
+      },
+    ],
+  }
+  // {
+  //   timestamps: { createdAt: "created_at", updatedAt: "updated_at" }, //to be able to sort by most recent messaage
+  // }
+);
+
+const Chat = model("Chat", ChatSchema);
+
+module.exports = Chat;
---
+++ models/Item.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const ItemSchema = new Schema({
+  name: String,
+  img: String,
+  type: enum[("Service", "Item")],
+  category: enum[("Furniture", "Plants", "Food")],
+  description: String,
+  status: enum[("Available", "Pending", "Sold")],
+  favourites: Number,
+  owner: { type: Schema.Types.ObjectId, ref: "User" },
+  // Optional:
+  // Comments from other Users
+});
+
+const Item = model("Item", ItemSchema);
+
+module.exports = Item;
