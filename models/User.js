a359be5a1b52cf7ae492a462914ead967aae7bba
Karl Secen <karl.secen@gmail.com>
2020-05-13 15:07:50 +0200

    package installs

---
+++ models/User.js
@@ -0,0 +1,36 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  profileImg: String,
+  location: {
+    type: String,
+    enum: [
+      "Prenzlauer Berg",
+      "Mitte",
+      "Kreuzberg"
+    ]
+  },
+  inventory: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Items'
+  }],
+  messages: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Chat'
+  }],
+  favourites: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Items'
+  }],
+
+
+
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
