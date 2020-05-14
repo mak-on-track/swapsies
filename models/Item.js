f9f91eab2d9705e52bcf121f9b42891df646fb00
emelko <ml.kochsiek@mailbox.org>
2020-05-14 11:37:09 +0200

    Fix item model and routes

--- models/Item.js
+++ models/Item.js
@@ -4,11 +4,25 @@ const { Schema, model } = mongoose;
 const ItemSchema = new Schema({
   name: String,
   img: String,
-/*   type: enum[("Service", "Item")],
- *//*   category: enum[("Furniture", "Plants", "Food")],
- */  description: String,
-/*   status: enum[("Available", "Pending", "Sold")],
- */  favourites: Number,
+  service: Boolean,
+  category: {
+    type: String,
+    enum: [
+      "Furniture",
+      "Plants",
+      "Food"
+    ]
+  },
+  description: String,
+  status: {
+    type: String,
+    enum: [
+      "Available", 
+      "Pending", 
+      "Sold"
+    ]
+  },
+  favourites: Number,
   owner: { type: Schema.Types.ObjectId, ref: "User" },
   // Optional:
   // Comments from other Users
