a2b5aeb7341915b605e9461f942b50ae4914c174
Aleah Connley <aleahkc@gmail.com>
2020-05-14 17:51:03 +0200

    updated

--- client/src/components/EditProfile.jsx
+++ client/src/components/EditProfile.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class EditProfile extends Component {
   state = {};
   render() {
+    console.log(this.props);
     return <div>Edit a profile</div>;
   }
 }
