dcad047a350c6e0575bfd455eca2ea8e7433ca06
Aleah Connley <aleahkc@gmail.com>
2020-05-14 14:05:17 +0200

    routes fixed

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+// import './index.css';
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
+import * as serviceWorker from "./serviceWorker";
 
-import * as serviceWorker from './serviceWorker';
-
-axios.get('/api/auth/loggedin')
-  .then(response => {
-    const user = response.data;
-    ReactDOM.render(
-      <BrowserRouter>
-        <App user={user} />
-      </BrowserRouter>,
-      document.getElementById('root')
-    );
-  })
+axios.get("/api/auth/loggedin").then((response) => {
+  const user = response.data;
+  ReactDOM.render(
+    <BrowserRouter>
+      <App user={user} />
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
