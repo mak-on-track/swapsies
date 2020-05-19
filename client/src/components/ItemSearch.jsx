23bf1aa2235c1436855fca9145d830b7bf1b1e91
emelko <ml.kochsiek@mailbox.org>
2020-05-19 16:46:13 +0200

    Add form styling and list items as Bulma card

--- client/src/components/ItemSearch.jsx
+++ client/src/components/ItemSearch.jsx
@@ -73,20 +73,10 @@ class ItemSearch extends Component {
 
   render() {
     const userId = this.props.user._id;
-    // console.log(this.props.user, "props user");
-    // console.log(this.state.location, "statae location");
-    // console.log(this.state.locationOptions, "location optn");
     const location = this.state.location;
     const locationOptions = this.state.locationOptions;
 
     const filteredItems = this.props.itemsList.filter((item) => {
-      /*       console.log(
-        item.location,
-        "item location",
-        this.state.location,
-        "state loctn"
-      ); */
-
       if (
         item.location !== this.state.location &&
         this.state.location !== "Select Kiez" &&
@@ -141,31 +131,43 @@ class ItemSearch extends Component {
     const displayThings = statusThings.map((thing) => {
       return (
         <div className="main">
-          <hr />
-          <ul>
-            <li>Photo (need to start cloudinary)</li>
-            <li>Category: {thing.category}</li>
-            <li>Name: {thing.name}</li>
-            <li>Description: {thing.description}</li>
-            <li>Status: {thing.status}</li>
-            <li>Favourites: {thing.favourites}</li>
-            <li>Location: {thing.location}</li>
-            <li>
-              <button type="button">Add to Favourites</button>
-            </li>
-            {thing.owner._id === userId ? (
-              <li>Posted by You!</li>
-            ) : (
-              <li>
-                Posted by:
-                <Link to={`/user/${thing.owner._id}`}>
-                  {thing.owner.username}
-                </Link>
-              </li>
-            )}
-            <button type="button">Message User</button>
-          </ul>
-        </div>
+          <div className="card">
+            <div className="card-content">
+              <div className="media" style={{"marginBottom":"0.7rem"}}>
+                <div className="media-left">
+                  <figure className="image is-48x48">
+                    <img src="https://images.unsplash.com/photo-1551298698-66b830a4f11c" alt="Red room"/>
+                  </figure>
+                </div>
+                <div className="media-content">
+                  <p className="title is-4">
+                    <a href="/">{thing.name}</a> 
+                  </p>
+                  <p className="subtitle is-6" style={{"marginBottom":"0.5rem"}}>{thing.location}</p>
+                  <p className="subtitle is-6" style={{"marginBottom":"0.5rem"}}>Favourites: {thing.favourites}</p>
+                </div>
+              </div>
+
+              <div className="content">
+                {thing.description}<br/>Status: {thing.status}
+                {thing.owner._id === userId ? (
+                    <p>My post</p>
+                  ) : (
+                    <p>
+                      Post by<> </>
+                      <a href="{`/user/${thing.owner._id}`}">
+                        {thing.owner.username}
+                      </a>
+                    </p>
+                )}
+              </div>
+            </div>
+            <footer class="card-footer">
+              <a href="#" class="card-footer-item">Add to Favourites</a>
+              <a href="#" class="card-footer-item">Message User</a>
+            </footer>
+          </div>
+      </div>
       );
     });
 
@@ -173,128 +175,168 @@ class ItemSearch extends Component {
       /*       console.log(service); */
       return (
         <div className="main">
-          <hr />
-          <ul>
-            <li>Name: {service.name}</li>
-            <li>Location: {service.location}</li>
-            <li>Description: {service.description}</li>
-            <li>Favourites: {service.favourites}</li>
-            <li>
-              <button type="button">Add to Favourites</button>
-            </li>
-            {service.owner._id === userId ? (
-              <li>Posted by You!</li>
-            ) : (
-              <li>
-                Posted by:
-                <Link to={`/user/${service.owner._id}`}>
-                  {service.owner.username}
-                </Link>
-              </li>
-            )}
-            <button type="button">Message User</button>
-          </ul>
+          <div className="card">
+            <div className="card-content">
+              <div className="media" style={{"marginBottom":"0.7rem"}}>
+                <div className="media-left">
+                  <figure className="image is-48x48">
+                    <img src="https://images.unsplash.com/photo-1551298370-9d3d53740c72" alt="Yellow chair"/>
+                  </figure>
+                </div>
+                <div className="media-content">
+                  <p className="title is-4">
+                    <a href="/">{service.name}</a> 
+                  </p>
+                  <p className="subtitle is-6" style={{"marginBottom":"0.5rem"}}>{service.location}</p>
+                  <p className="subtitle is-6" style={{"marginBottom":"0.5rem"}}>Favourites: {service.favourites}</p>
+                </div>
+              </div>
+
+              <div className="content">
+                {service.description}
+                {service.owner._id === userId ? (
+                    <p>My post</p>
+                  ) : (
+                    <p>
+                      Post by<> </>
+                      <a href="{`/user/${service.owner._id}`}">
+                        {service.owner.username}
+                      </a>
+                    </p>
+                )}
+              </div>
+            </div>
+            <footer class="card-footer">
+              <a href="#" class="card-footer-item">Add to Favourites</a>
+              <a href="#" class="card-footer-item">Message User</a>
+            </footer>
+          </div>
         </div>
       );
     });
 
     return (
       <div className="main">
-        <h2>What are you looking for?</h2>
-        <select id="type" onChange={this.handleTypeSelect}>
-          <option value="">Select Thing or Service</option>
-          <option value="Thing">Thing</option>
-          <option value="Service">Service</option>
-        </select>
+        <label className="label">What are you looking for?</label>
+        <div className="field">
+          <div className="control">
+            <div className="select"> 
+              <select id="itemtype" name="itemtype" onChange={this.handleTypeSelect}>
+                <option value="">Select</option>
+                <option value="Thing">Thing</option>
+                <option value="Service">Service</option>
+              </select>
+            </div>
+          </div>
+        </div>
 
         {this.state.type === "Thing" && (
           <>
-            <h2>Find by Category</h2>
-            <select id="category" onChange={this.handleCategorySelect}>
-              <option value="">All</option>
-              <option value="Plants">Plants</option>
-              <option value="Furniture">Furniture</option>
-            </select>
+            <label className="label">Find by Category</label>
+            <div className="field">
+              <div className="control">
+                <div className="select"> 
+                  <select id="category" onChange={this.handleCategorySelect}>
+                    <option value="">All</option>
+                    <option value="Plants">Plants</option>
+                    <option value="Furniture">Furniture</option>
+                  </select>
+                </div>
+              </div>
+            </div>
           </>
         )}
 
         {this.state.type !== "" && (
           <>
-            <form>
+             <div className="field has-addons">
+               <div className="control">
+                <input
+                  className="input"
+                  name="search"
+                  type="text"
+                  placeholder="Search"
+                  id="search"
+                  value={this.state.search}
+                  onChange={this.handleSearchChange}
+                />
+              </div>
+               <div className="control">
+                <a className="button">
+                  Search
+                </a>
+              </div>
+            </div>
+          </>
+        )}
+
+        {this.state.type !== "" && (
+          <>
+            <label className="label">Location:</label>
+            <div className="field">
+              <div className="control">
+                <div className="select"> 
+                  <select id="location" name="location" value={location} onChange={this.handleKiezSelect}>
+                    {locationOptions.map((option) => {
+                      return (
+                        <option value={option} key={option}>
+                          {option}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+
+        {this.state.type === "Thing" && (
+          <>
+            <label className="checkbox" style={{"marginRight":"1rem"}}>
               <input
-                type="text"
-                placeholder="Search"
-                name="search"
-                id="search"
-                value={this.state.search}
-                onChange={this.handleSearchChange}
+                type="checkbox"
+                name="Available"
+                id="available"
+                checked={this.state.availableCheck}
+                onChange={this.handleAvailableCheck}
               />
-            </form>
+              Available
+            </label>
+          
+            <label className="checkbox" style={{"marginRight":"1rem"}}>
+              <input
+                type="checkbox"
+                name="Reserved"
+                id="reserved"
+                checked={this.state.reservedCheck}
+                onChange={this.handleReservedCheck}
+              />
+              Reserved
+            </label>
+
+            <label className="checkbox" style={{"marginRight":"1rem"}}>
+              <input
+                type="checkbox"
+                name="Swapped"
+                id="swapped"
+                checked={this.state.swappedCheck}
+                onChange={this.handleSwappedCheck}
+              />
+              Swapped
+            </label>
           </>
         )}
 
-        {this.state.type !== "" && (
-          <>
-            <label>Location:</label>
-            <select
-              name="location"
-              value={location}
-              onChange={this.handleKiezSelect}
-            >
-              {locationOptions.map((option) => {
-                return (
-                  <option value={option} key={option}>
-                    {option}
-                  </option>
-                );
-              })}
-            </select>
-          </>
-        )}
-
-        {this.state.type === "Thing" && (
-          <>
-            <label htmlFor="available">Available</label>
-            <input
-              type="checkbox"
-              name="Available"
-              id="available"
-              checked={this.state.availableCheck}
-              onChange={this.handleAvailableCheck}
-            />
-            <label htmlFor="reserved">Reserved</label>
-            <input
-              type="checkbox"
-              name="Reserved"
-              id="reserved"
-              checked={this.state.reservedCheck}
-              onChange={this.handleReservedCheck}
-            />
-            <label htmlFor="swapped">Swapped</label>
-            <input
-              type="checkbox"
-              name="Swapped"
-              id="swapped"
-              checked={this.state.swappedCheck}
-              onChange={this.handleSwappedCheck}
-            />
-          </>
-        )}
-
-        <hr />
         <div>
           {this.state.type === "Thing" && (
-            <>
-              <h4>Things</h4>
-              <div className="itemslist">{displayThings}</div>
-            </>
+            <div className="itemslist">{displayThings}</div>
           )}
         </div>
-        <hr />
         <div>
           {this.state.type === "Service" && (
             <>
-              <h4>Services</h4>
+              {/* <h4>Services</h4> */}
               <div className="serviceList">{displayServices}</div>
             </>
           )}
